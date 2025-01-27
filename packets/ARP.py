d89d58464ee8fea2c7a220c98d44a8ecb1a665ce
Chris Tenday <ctenday@gmail.com>
2025-01-27 18:11:09 +0100

    Ethernet frame & layer

--- packets/ARP.py
+++ packets/ARP.py
@@ -9,47 +9,53 @@ class ARP(Packet):
     _hardwareSize = struct.pack("!B", 6)  #size of the mac address in bytes.
     _protocolSize = struct.pack("!B", 4)  #size of the protocol address to resolve (IPv4)
 
-    opCode = None #specifies whether the ARP packet is an query or reply, or even reserve-arp
+    opCode = None  #specifies whether the ARP packet is an query or reply, or even reserve-arp
     senderMac = None
     senderIP = None
     targetMac = "00:00:00:00:00:00"
     targetIP = None
-    _interface=None
+    _interface = None
 
     def __init__(self, interface):
         self._interface = interface
 
-    #set operation code (ARP query or reply)
+    '''set operation code (ARP query or reply)'''
     def setOpCode(self, opCode):
         self.opCode = struct.pack("!H", opCode)
 
-    #Specify the ARP operation type (request, reply or reverse) by means of a string.
-    def setOperationType(self, type = "request"):
+    '''
+    Specify the ARP operation type (request, reply or reverse) by means of a string.
+    '''
+    def setOperationType(self, type="request"):
         if type == "request":
-            self.opCode=1
+            self.opCode = 1
         elif type == "reply":
-            self.opCode=2
+            self.opCode = 2
         self.opCode = struct.pack("!H", self.opCode)
 
-    #set the target IP.
+    '''
+    set the target IP.
+    '''
     def setTargetIP(self, targetIp):
         self.targetIP = packIP(targetIp)
 
-    #Set the target Mac to resolve.
+    '''Set the target Mac to resolve.'''
     def setTargetMac(self, mac):
         self.targetMac = packMacAddress(mac)
 
-    #Set the sending device IP
-    #By default: Device IP is used.
+    '''Set the sending device IP
+    By default: Device IP is used.
+    '''
     def setSenderIP(self, senderIP):
         self.senderIP = packIP(senderIP)
 
-    #Set the sending device Mac.
-    #By default : Device interface Mac is used.
+    '''Set the sending device Mac.
+    By default : Device interface Mac is used.
+    '''
     def setSenderMac(self, mac):
         self.senderMac = packMacAddress(mac)
 
-    #Build the packet.
+    '''Build the packet.'''
     def build(self):
         self.senderIP = packIP(getDeviceIP()) if self.senderIP is None else self.senderIP
         self.senderMac = packMacAddress(getInterfaceMac(self._interface)) if self.senderMac is None else self.senderMac
--- packets/EthernetFrame.py
+++ packets/EthernetFrame.py
@@ -4,35 +4,39 @@ from packets.ARP import *
 
 
 class EthernetFrame(Packet):
-    _sourceMac = None  #source Mac address.
-    _destMac = None  #destination Mac address.
-    _frameType = None  #frame type (ARP,RARP or IP)
-    _payload = None  #frame frame payload
+    _sourceMac = None  #source Mac address.(6-bytes)
+    _destMac = None  #destination Mac address.(6-bytes)
+    _frameType = None  #frame type (ARP,RARP or IP) (2-bytes)
+    _payload = None  #frame payload
     _interface=None #network interface
 
     def __init__(self, interface):
         self._interface = interface
 
-    #Source Mac setting
+    '''Source Mac setting'''
     def setSourceMac(self, mac):
         self._sourceMac = packMacAddress(mac)
 
-    #Dest Mac setting
+    '''Dest Mac setting'''
     def setDestMac(self, mac):
         self._destMac = packMacAddress(mac)
 
-    #Set the paylod of the frame.
+    '''Set the payload of the frame.'''
     def setPayload(self, payload):
-        # set frame type based on the payload
+        """
+        set frame type based on the payload
+        """
         if isinstance(payload, ARP) and issubclass(ARP, Packet):
-            self._frameType = struct.pack("!H", 1)
+            self._frameType = struct.pack("!H", 0x0806)
         elif type(payload) == "IPDatagram":
             self._frameType = struct.pack("!H", 2)
         else:
             raise Exception("Payload not supported.")
         self._payload = payload
 
-    #Build and Get the packet.
+    """
+    Build and Get the packet.
+    """
     def build(self):
         #fill source Mac
         self._sourceMac = packMacAddress(getInterfaceMac(self._interface)) if self._sourceMac is None else self._sourceMac
